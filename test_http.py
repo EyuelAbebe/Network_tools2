c6c0a95dd7e6d9d244856f61464ec34f9c0b05da
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-06-11 15:21:08 -0700

    Fixed the test function to work with the Http response change we made on the server.py

--- test_http.py
+++ test_http.py
@@ -7,31 +7,31 @@ def test_200():
 
     _client = Client()
     _client.send("GET / HTTP/1.1")
-    assert _client.receive() == "200 OK"
+    assert _client.receive() == 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n200 OK'
 
 
 def test_400():
 
     _client = Client()
     _client.send("GET HTTP/1.1")
-    assert _client.receive() == "400 Bad Request"
+    assert _client.receive() == 'HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\n400 Bad Request'
 
 
 def test_405():
 
     _client = Client()
     _client.send("PUT / HTTP/1.1")
-    assert _client.receive() == "405 Method Not Allowed"
+    assert _client.receive() == 'HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain\r\n\r\n405 Method Not Allowed'
     _client = Client()
     _client.send("HEAD / HTTP/1.1")
-    assert _client.receive() == "405 Method Not Allowed"
+    assert _client.receive() == 'HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain\r\n\r\n405 Method Not Allowed'
 
 
 def test_505():
 
     _client = Client()
     _client.send("GET / HTTP/1.0")
-    assert _client.receive() == "505 HTTP Version Not Supported"
+    assert _client.receive() == 'HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Type: text/plain\r\n\r\n505 HTTP Version Not Supported' 
 
 
 def test_404():
